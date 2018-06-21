f774495f9b917d0c6a9130259a97f177482ceb6f
cscawley <cscawley@mac.com>
2018-06-20 21:56:18 -0500

    initial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.materialboxed').materialbox();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
