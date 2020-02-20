34801a89dcc347fd72c7e341318998eb3c21f680
bartosjiri <me@bartosjiri.com>
2020-02-20 21:33:08 +0100

    Update layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,23 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Head from './head/Head'
-import Wrapper from './wrapper/Wrapper'
-import Background from '../components/background/Background';
+import Background from '../components/background/Background'
 import Header from './header/Header'
-// import Main from './main/Main'
+import Main from './main/Main'
 import Footer from './footer/Footer'
 
 const Layout = ({ children }) => (
   <>
     <Head />
-    <Wrapper>
-      <Header />
-      <main>
-        {children}
-      </main>
-      <Footer />
-    </Wrapper>
-    <Background />
+    <Header />
+    <Main>
+      {children}
+    </Main>
+    <Footer />
+    {/* <Background /> */}
   </>
 )
 
