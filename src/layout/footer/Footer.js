0ed2ad6c5c209cac569b9ae9fae7f4bb204a7c59
bartosjiri <me@bartosjiri.com>
2020-03-01 11:07:36 +0100

    Update content

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -11,30 +11,37 @@ import secondaryButtonData from '../../config/buttons/secondary.yaml'
 const Footer = () => (
   <footer className={footerStyle.footer}>
     <div className={footerStyle.group}>
-      {primaryButtonData.visible && (
-        <Button
-          href={primaryButtonData.url}
-          target={primaryButtonData.tab && '_blank'}
-          primary
-          disabled={primaryButtonData.disabled}
-        >
-          <span>{primaryButtonData.text}</span>
-          <SVG src={`/icons/${primaryButtonData.icon}`} />
-        </Button>
-      )}
+      {primaryButtonData
+        .sort((a, b) => ((a.priority > b.priority) ? 1 : -1))
+      .map(button => (
+        button.visible && (
+            <Button
+              href={button.url}
+              target={button.tab && '_blank'}
+              primary
+              disabled={button.disabled}
+            >
+              <span>{button.text}</span>
+              <SVG src={`/icons/${button.icon}`} />
+            </Button>
+          )
+        ))
+      }
     </div>
+
+
     <div className={footerStyle.group}>
       {secondaryButtonData
         .sort((a, b) => ((a.priority > b.priority) ? 1 : -1))
         .map(button => (
           button.visible && (
-          <Button
-            key={button.name}
-            href={button.url}
-            target={button.tab && '_blank'}
-          >
-            <SVG src={`/icons/${button.icon}`} />
-          </Button>
+            <Button
+              key={button.name}
+              href={button.url}
+              target={button.tab && '_blank'}
+            >
+              <SVG src={`/icons/${button.icon}`} />
+            </Button>
           )
         ))}
     </div>
