c179cb095e09ca7b2b2b9e609ceff5b3ab442b4d
bartosjiri <me@bartosjiri.com>
2020-07-13 22:01:17 +0200

    Update header

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 
 import headerStyle from './assets/Header.module.scss'
 
 const Header = () => (
   <header className={headerStyle.header}>
     <Link to="/">
-      <h1>Purple World</h1>
+      {/* <h1>Purple World</h1> */}
+      <img src="logo/logo.png" alt="logo" />
     </Link>
   </header>
 )
