bacad9ed2afc95a7ed5ccab8e9a89e6874ec0080
bartosjiri <me@bartosjiri.com>
2020-07-14 21:14:26 +0200

    Update logo

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -3,11 +3,13 @@ import {Link} from 'gatsby'
 
 import headerStyle from './assets/Header.module.scss'
 
+import logoImage from "../../../static/logo/logo.png"
+
 const Header = () => (
   <header className={headerStyle.header}>
     <Link to="/">
       {/* <h1>Purple World</h1> */}
-      <img src="logo/logo.png" alt="logo" />
+      <img src={logoImage} alt="logo" />
     </Link>
   </header>
 )
