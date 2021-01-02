23118a6e088514fa40d59370606a8c78da6a61e3
bartosjiri <me@bartosjiri.com>
2021-01-02 18:30:30 +0100

    Update background component

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -1,14 +1,54 @@
 import React from 'react'
 
+import Image1 from '../../../static/background/image1.jpg'
+import Image2 from '../../../static/background/image2.jpg'
+import Image3 from '../../../static/background/image3.jpg'
+import Image4 from '../../../static/background/image4.jpg'
+
 import backgroundStyle from './assets/Background.module.scss'
 
-const Background = () => (
-  <ul className={backgroundStyle.background}>
-    <li />
-    <li />
-    <li />
-    <li />
-  </ul>
-)
+const Background = () => {
+  const imagesList = [Image1, Image2, Image3, Image4]
+
+  const animationName = 'backgroundAnimation'
+  const animatonKeyframes = `
+    @keyframes ${animationName} {
+      0% {
+        transform: translateX(-5%);
+        opacity: 0;
+      }
+      ${5 * 4 / imagesList.length}% {
+        opacity: 0.25;
+      }
+      ${25 * 4 / imagesList.length}% {
+        opacity: 0.25;
+      }
+      ${30 * 4 / imagesList.length}% {
+        opacity: 0;
+        transform: translateX(5%);
+      }
+    }
+  `
+
+  return (
+    <ul className={backgroundStyle.background}>
+      <style>{animatonKeyframes}</style>
+      {imagesList && imagesList.length && imagesList.map((item, i) => (
+        <li
+          key={item}
+          style={{
+            animationName,
+            animationDuration: `${imagesList.length * 10}s`,
+            animationDelay: `${i * 10}s`,
+            animationTimingFunction: 'linear',
+            animationIterationCount: 'infinite'
+          }}
+        >
+          <img src={item} alt={`Background ${i + 1}`} />
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default Background
