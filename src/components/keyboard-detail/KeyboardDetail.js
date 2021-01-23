53d116565e3238107b08d8234fc9271317b86576
bartosjiri <me@bartosjiri.com>
2021-01-23 10:48:22 +0100

    Update keybindings components & config

--- src/components/keyboard-detail/KeyboardDetail.js
+++ src/components/keyboard-detail/KeyboardDetail.js
@@ -9,26 +9,28 @@ import keyStyle from '../keyboard/Key.module.scss'
 const KeyboardDetail = ({currentKey}) => {
   return (
     <div className={style.detail}>
-      <div className={`${style.key} ${keyStyle.key}`}>
-        <div className={`${style.inner} ${keyStyle.inner} ${currentKey ? style.active : ''}`}>
-          <div className={keyStyle.label}>
-            <span>{currentKey ? currentKey.key : 'No key selected'}</span>
-          </div>
-          <div className={keyStyle.actions}>
-            {currentKey?.keybinds && currentKey.keybinds.map(keybind => (
-              <div
-                key={nanoid()}
-                className={`${style.action_item} ${keyStyle.item}`}
-                style={{color: tagsConfig.find(t => t.tag === keybind.tags[0])?.color}}
-              >
-                <div className={keyStyle.tags}>
-                  {keybind.modifier && (
-                    <span>{keybind.modifier}+</span>
-                  )}
+      <div className={style.row}>
+        <div className={`${style.key} ${keyStyle.key}`}>
+          <div className={`${style.inner} ${keyStyle.inner} ${currentKey ? style.active : ''}`}>
+            <div className={keyStyle.label}>
+              <span>{currentKey ? currentKey.key : 'No key selected'}</span>
+            </div>
+            <div className={keyStyle.actions}>
+              {currentKey?.keybinds && currentKey.keybinds.map(keybind => (
+                <div
+                  key={nanoid()}
+                  className={`${style.action_item} ${keyStyle.item}`}
+                  style={{color: tagsConfig.find(t => t.tag === keybind.tags[0])?.color}}
+                >
+                  <div className={keyStyle.tags}>
+                    {keybind.modifier && (
+                      <span>{keybind.modifier}+</span>
+                    )}
+                  </div>
+                  <span>{keybind.desc}</span>
                 </div>
-                <span>{keybind.desc}</span>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/components/keyboard-options/KeyboardOptions.js
+++ src/components/keyboard-options/KeyboardOptions.js
@@ -1,5 +1,6 @@
 import React, {memo, useState} from 'react'
 import SVG from 'react-inlinesvg'
+import OutsideClickHandler from 'react-outside-click-handler'
 
 import Button from '../button/Button'
 
@@ -19,29 +20,31 @@ const KeyboardOptions = memo(({activeTags, tagsEvent}) => {
 
   return (
     <div className={style.options}>
-      <div className={`${style.select} ${open ? style.active : ''}`}>
-        <Button
-          className={style.button}
-          onClick={() => setOpen(!open)}
-          primary
-        >
-          <span>Select filters</span>
-        </Button>
-        {tagsData && tagsData.length && (
-          <div className={style.options}>
-            {tagsData.map(option => (
-              <Button
-                key={option.tag}
-                className={style.option}
-                onClick={() => handleOptionSelect(option.tag)}
-              >
-                <SVG src={`/icons/${activeTags.includes(option.tag) ? 'checkmark' : 'crossmark'}.svg`} />
-                <span style={{color: option.color ? option.color : 'inherit'}}>{option.label}</span>
-              </Button>
-            ))}
-          </div>
-        )}
-      </div>
+      <OutsideClickHandler onOutsideClick={() => setOpen(false)}>
+        <div className={`${style.select} ${open ? style.active : ''}`}>
+          <Button
+            className={style.button}
+            onClick={() => setOpen(!open)}
+            primary
+          >
+            <span>Select filters</span>
+          </Button>
+          {tagsData && tagsData.length && (
+            <div className={style.options}>
+              {tagsData.map(option => (
+                <Button
+                  key={option.tag}
+                  className={style.option}
+                  onClick={() => handleOptionSelect(option.tag)}
+                >
+                  <SVG src={`/icons/${activeTags.includes(option.tag) ? 'checkmark' : 'crossmark'}.svg`} />
+                  <span style={{color: option.color ? option.color : 'inherit'}}>{option.label}</span>
+                </Button>
+              ))}
+            </div>
+          )}
+        </div>
+      </OutsideClickHandler>
     </div>
   )
 })
