f1e7c9178460a5f3750e9a3ea1f60742e5f9a834
bartosjiri <me@bartosjiri.com>
2021-02-26 17:23:18 +0100

    Update preloader

--- static/scripts/preloader.js
+++ static/scripts/preloader.js
@@ -4,12 +4,8 @@ document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
     body.classList.add('preloader_ready')
     setTimeout(function () {
-      body.classList.add('preloader_scroll')
-    }, 500)
-    setTimeout(function () {
       body.classList.remove('preloader_active')
       body.classList.remove('preloader_ready')
-      body.classList.remove('preloader_scroll')
-    }, 1000)
+    }, 500)
   }
 }
